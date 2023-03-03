ff0e39f25f403577781130eb1a42cb2b1bfd8218
yrk721 <yoorae0721@naver.com>
2023-03-03 14:53:57 +0900

    Update _app.tsx

--- assignment_freeboard/pages/_app.tsx
+++ assignment_freeboard/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: "http://backendonline.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+export default MyApp;
