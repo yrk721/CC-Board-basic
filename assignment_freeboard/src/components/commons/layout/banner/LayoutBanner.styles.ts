738da36522a8e4824dceb3b40aa1a368222f2ca5
yrk721 <yoorae0721@naver.com>
2023-03-04 07:18:41 +0900

    Create LayoutBanner.styles.ts - 배너 스타일 설정

---
+++ assignment_freeboard/src/components/commons/layout/banner/LayoutBanner.styles.ts
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+export const CarouselWrapper = styled.div`
+  background: linear-gradient(
+    95.18deg,
+    #6400ff 0.47%,
+    #e3d1ff 102.52%,
+    #d0b1ff 102.52%
+  );
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+  height: 350px;
+  margin: 20px 20px 20px 0px;
+`;
+
+export const SliderItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 320px;
+  font-family: "SUIT";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 70px;
+  line-height: 50px;
+  text-align: center;
+  letter-spacing: -0.03em;
+  color: #ffffff;
+  img {
+    height: 80px;
+  }
+`;
