40a4acf4d2915e3553204aede914857b5c36f159
yrk721 <yoorae0721@naver.com>
2023-03-04 07:19:35 +0900

    Create BoardDetail.presenter.tsx

---
+++ assignment_freeboard/src/components/units/board/detail/BoardDetail.presenter.tsx
@@ -0,0 +1,54 @@
+import { Tooltip } from "antd";
+import { getDate } from "../../../../commons/libraries/utils";
+import * as S from "./BoardDetail.styles";
+import { IBoardDetailUIProps } from "./BoardDetail.types";
+
+export default function BoardDetailUI(props: IBoardDetailUIProps) {
+  return (
+    <S.Wrapper>
+      <S.CardWrapper>
+        <S.Header>
+          <S.AvatarWrapper>
+            <S.Avatar src="/images/avatar.png" />
+            <S.Info>
+              <S.Writer>{props.data?.fetchBoard?.writer}</S.Writer>
+              <S.CreatedAt>
+                {getDate(props.data?.fetchBoard?.createdAt)}
+              </S.CreatedAt>
+            </S.Info>
+          </S.AvatarWrapper>
+          <S.IconWrapper>
+            <Tooltip
+              placement="topRight"
+              title={`${props.data?.fetchBoard.boardAddress?.address ?? ""} ${
+                props.data?.fetchBoard.boardAddress?.addressDetail ?? ""
+              }`}
+            ></Tooltip>
+          </S.IconWrapper>
+        </S.Header>
+        <S.Body>
+          <S.Title>{props.data?.fetchBoard?.title}</S.Title>
+          <S.Contents>{props.data?.fetchBoard?.contents}</S.Contents>
+
+          <S.LikeWrapper>
+            <S.IconWrapper>
+              <S.LikeIcon onClick={props.onClickLike} />
+              <S.LikeCount>{props.data?.fetchBoard.likeCount}</S.LikeCount>
+            </S.IconWrapper>
+            <S.IconWrapper>
+              <S.DislikeIcon onClick={props.onClickDislike} />
+              <S.DislikeCount>
+                {props.data?.fetchBoard.dislikeCount}
+              </S.DislikeCount>
+            </S.IconWrapper>
+          </S.LikeWrapper>
+        </S.Body>
+      </S.CardWrapper>
+      <S.BottomWrapper>
+        <S.Button onClick={props.onClickMoveToBoardList}>글목록</S.Button>
+        <S.Button onClick={props.onClickMoveToBoardEdit}>수정</S.Button>
+        <S.Button>삭제</S.Button>
+      </S.BottomWrapper>
+    </S.Wrapper>
+  );
+}
