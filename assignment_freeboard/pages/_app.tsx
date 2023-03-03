013518a47a0241c3a130d3fc754fdd7b756cdc38
yrk721 <yoorae0721@naver.com>
2023-03-03 21:16:12 +0900

    Update _app.tsx

--- assignment_freeboard/pages/_app.tsx
+++ assignment_freeboard/pages/_app.tsx
@@ -1,17 +1,20 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+// import "antd/dist/antd.css";
+import { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout/";
+import ApolloSetting from "../src/components/commons/apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: "http://backendonline.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ApolloSetting>
+      <>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    </ApolloSetting>
   );
 }
 
