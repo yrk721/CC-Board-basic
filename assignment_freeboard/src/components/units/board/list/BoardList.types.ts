a36a66615dec3c78196e830fa044569d7f2a2400
yrk721 <yoorae0721@naver.com>
2023-03-04 07:19:58 +0900

    Create BoardList.types.ts

---
+++ assignment_freeboard/src/components/units/board/list/BoardList.types.ts
@@ -0,0 +1,20 @@
+import { ApolloQueryResult } from "@apollo/client";
+import { MouseEvent } from "react";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../../commons/types/generated/types";
+
+export interface IBoardListUIProps {
+  data?: Pick<IQuery, "fetchBoards">;
+  onClickMoveToBoardNew: () => void;
+  onClickMoveToBoardDetail: (event: MouseEvent<HTMLDivElement>) => void;
+  refetch: (
+    variables?: Partial<IQueryFetchBoardsArgs> | undefined
+  ) => Promise<ApolloQueryResult<Pick<IQuery, "fetchBoards">>>;
+  count?: number;
+}
+
+export interface ITextTokenProps {
+  isMatched: boolean;
+}
