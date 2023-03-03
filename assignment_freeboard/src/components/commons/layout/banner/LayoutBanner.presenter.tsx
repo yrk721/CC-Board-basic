7390e62fa515b73defe77469a619af376d9abc9a
yrk721 <yoorae0721@naver.com>
2023-03-04 07:17:47 +0900

    Create LayoutBanner.presenter.tsx - 배너 UI 캐러셀 구현

---
+++ assignment_freeboard/src/components/commons/layout/banner/LayoutBanner.presenter.tsx
@@ -0,0 +1,39 @@
+import { CarouselWrapper, SliderItem } from "./LayoutBanner.styles";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function LayoutBannerUI() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <CarouselWrapper>
+      <Slider {...settings}>
+        <div>
+          <SliderItem>
+            <p>Carousel</p>
+            <img src="/images/rocket.png" />
+          </SliderItem>
+        </div>
+        <div>
+          <SliderItem>
+            <p>Carousel</p>
+            <img src="/images/rocket.png" />
+          </SliderItem>
+        </div>
+        <div>
+          <SliderItem>
+            <p>Carousel</p>
+            <img src="/images/rocket.png" />
+          </SliderItem>
+        </div>
+      </Slider>
+    </CarouselWrapper>
+  );
+}
