c1697496ca5355c4d11c554614b91da595d6689e
yrk721 <yoorae0721@naver.com>
2023-03-04 07:19:45 +0900

    Create BoardList.container.tsx

---
+++ assignment_freeboard/src/components/units/board/list/BoardList.container.tsx
@@ -0,0 +1,40 @@
+import BoardListUI from "./BoardList.presenter";
+import { useQuery } from "@apollo/client";
+import { FETCH_BOARDS, FETCH_BOARDS_COUNT } from "./BoardList.queries";
+import { useRouter } from "next/router";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+  IQueryFetchBoardsCountArgs,
+} from "../../../../commons/types/generated/types";
+import { MouseEvent } from "react";
+
+export default function BoardList() {
+  const router = useRouter();
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+  const { data: dataBoardsCount } = useQuery<
+    Pick<IQuery, "fetchBoardsCount">,
+    IQueryFetchBoardsCountArgs
+  >(FETCH_BOARDS_COUNT);
+
+  const onClickMoveToBoardNew = () => {
+    void router.push("/boards/new");
+  };
+
+  const onClickMoveToBoardDetail = (event: MouseEvent<HTMLDivElement>) => {
+    void router.push(`/boards/${event.currentTarget.id}`);
+  };
+
+  return (
+    <BoardListUI
+      data={data}
+      onClickMoveToBoardNew={onClickMoveToBoardNew}
+      onClickMoveToBoardDetail={onClickMoveToBoardDetail}
+      refetch={refetch}
+      count={dataBoardsCount?.fetchBoardsCount}
+    />
+  );
+}
