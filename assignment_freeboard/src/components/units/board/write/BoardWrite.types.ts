fba3cb31de689c282647690f8420840582c69acd
yrk721 <yoorae0721@naver.com>
2023-03-04 05:41:48 +0900

    Create BoardWrite.types.ts

---
+++ assignment_freeboard/src/components/units/board/write/BoardWrite.types.ts
@@ -0,0 +1,37 @@
+import { ChangeEvent } from "react";
+import { IQuery } from "../../../../commons/types/generated/types";
+
+export interface IBoardWriteProps {
+  isEdit: boolean;
+  data?: Pick<IQuery, "fetchBoard">;
+}
+
+export interface IUpdateBoardInput {
+  title?: string;
+  contents?: string;
+}
+
+export interface ISubmitButtonProps {
+  isActive: boolean;
+}
+
+export interface IBoardWriteUIProps {
+  isActive: boolean;
+  writerError: string;
+  passwordError: string;
+  titleError: string;
+  contentsError: string;
+  onChangeWriter: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangePassword: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangeTitle: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangeContents: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  onClickSubmit: () => void;
+  onClickUpdate: () => void;
+  isEdit: boolean;
+  data?: Pick<IQuery, "fetchBoard">;
+  isOpen: boolean;
+}
+
+export interface ILayoutNavigationUIProps {
+  onClickMenu: (event: MouseEvent<HTMLDivElement>) => void;
+}
